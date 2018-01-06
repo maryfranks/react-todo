19c6a9ec0a768d2c7d5f6d1d6f4be21f599fe21e
Mary Franks <maryfranks@gmail.com>
2018-01-06 13:25:04 -0500

    style todo list

--- src/ListItem.js
+++ src/ListItem.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class ListItem extends Component {
   render() {
     return (
-      <div>
+
         <li>Make the List!</li>
-      </div>
+
     );
   }
 }
